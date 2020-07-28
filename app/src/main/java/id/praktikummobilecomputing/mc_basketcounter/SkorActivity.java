8c5a7d01ce02ea4e6a2fba280e70c6ac1b84debb
allen404 <gdikyallen@gmail.com>
2020-07-28 23:18:18 +0700

    rapihin kodeeeeee

--- app/src/main/java/id/praktikummobilecomputing/mc_basketcounter/SkorActivity.java
+++ app/src/main/java/id/praktikummobilecomputing/mc_basketcounter/SkorActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.TextView;
 
 
-public class CounterActivity extends AppCompatActivity {
+public class SkorActivity extends AppCompatActivity {
     int scoreA = 0;
     int scoreB = 0;
 
@@ -15,7 +15,7 @@ public class CounterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_counter);
+        setContentView(R.layout.activity_skor);
 
         String recNameTimA = getIntent().getStringExtra("TIM A");
         String recNameTimB = getIntent().getStringExtra("TIM B");
@@ -28,19 +28,19 @@ public class CounterActivity extends AppCompatActivity {
     }
 
     public void counter(View view) {
-        if (view.getId() == R.id.btn_score_one_tim_a) {
-            scoreA = scoreA + 1;
-        } else if (view.getId() == R.id.btn_score_two_tim_a) {
-            scoreA = scoreA + 2;
-        } else if (view.getId() == R.id.btn_score_three_tim_a) {
-            scoreA = scoreA + 3;
-        } else if (view.getId() == R.id.btn_score_one_tim_b) {
-            scoreB = scoreB + 1;
-        } else if (view.getId() == R.id.btn_score_two_tim_b) {
-            scoreB = scoreB + 2;
-        } else if (view.getId() == R.id.btn_score_three_tim_b) {
-            scoreB = scoreB + 3;
+        int id =view.getId();
+        switch(id){
+            case R.id.btn_score_one_tim_a: scoreA = scoreA + 1; break;
+            case R.id.btn_score_two_tim_a: scoreA = scoreA + 2; break;
+            case R.id.btn_score_three_tim_a: scoreA = scoreA + 3; break;
+            case R.id.btn_score_one_tim_b: scoreB = scoreB + 1; break;
+            case R.id.btn_score_two_tim_b: scoreB = scoreB + 2; break;
+            case R.id.btn_score_three_tim_b: scoreB= scoreB + 3; break;
+
+            case R.id.btn_reset_score_tim_a: scoreA = 0;
+            case R.id.btn_reset_score_tim_b: scoreB = 0;
         }
+
 
         TextView tvScoreTeamA = findViewById(R.id.tv_score_tim_a);
         TextView tvScoreTeamB = findViewById(R.id.tv_score_tim_b);
